4103724e1175c4386384e03e036e2ee44b56ef96
Ivan Zezyulya <ivan@zezyulya.com>
2016-11-30 09:56:31 +0600

    Add rename dependency, rename .rb file

--- Formula/prefix-change.rb
+++ Formula/prefix-change.rb
@@ -1,8 +1,10 @@
-class Prefixchange < Formula
+class PrefixChange < Formula
   desc "Rename prefix of iOS classes"
   homepage "https://github.com/ivanzoid/prefixChange"
   url "https://github.com/ivanzoid/prefixChange/archive/1.0.0.zip"
   sha256 "0d1d3cb594fae9cf27a6ce0a0de399b0b1ad21d03be8e2f6cf6dfd600a351b5f"
+
+  depends_on "rename"
 
   def install
     bin.install "prefixChange"
