cff7d3b337b049fdf9d9b62fe2b2e5cce6b8f593
Ivan Zezyulya <ivan@zezyulya.com>
2016-12-05 13:23:30 +0600

    prefixChange.rb: version -> 1.1

--- Formula/prefixChange.rb
+++ Formula/prefixChange.rb
@@ -1,8 +1,9 @@
 class Prefixchange < Formula
   desc "Rename prefix of iOS classes"
   homepage "https://github.com/ivanzoid/prefixChange"
-  url "https://github.com/ivanzoid/prefixChange/archive/1.0.0.zip"
-  sha256 "0d1d3cb594fae9cf27a6ce0a0de399b0b1ad21d03be8e2f6cf6dfd600a351b5f"
+  url "https://github.com/ivanzoid/prefixChange/archive/1.1.zip"
+  sha256 "2d3d19cc5fc8e0c48a1a64b15c70824dd99dac1e802d55011e5b5354611e5903"
+  version "1.1"
 
   depends_on "rename"
 
