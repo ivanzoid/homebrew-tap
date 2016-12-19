00e07a1bc31b16aae599e1c80b9a062e1baa2500
Ivan Zezyulya <ivan@IvanMac.local>
2016-12-19 18:17:46 +0600

    mergeXcodeproj 1.1

--- Formula/merge-xcodeproj.rb
+++ Formula/merge-xcodeproj.rb
@@ -1,9 +1,9 @@
 class MergeXcodeproj < Formula
   desc "Merge .xcodeproj"
   homepage "https://github.com/ivanzoid/mergeXcodeproj"
-  url "https://github.com/ivanzoid/mergeXcodeproj/archive/1.0.zip"
-  sha256 "9b8793432e1f5e0d6f0a182166e8eac7e8b742c1bb7adecd7cf1f427ed464247"
-  version "1.0"
+  url "https://github.com/ivanzoid/mergeXcodeproj/archive/1.1.zip"
+  sha256 "5cddd65c839f42888d4d069be28b2d4a840d2c57dbab7004cbbb1990600577e3"
+  version "1.1"
 
   depends_on "mergepbx"
 
